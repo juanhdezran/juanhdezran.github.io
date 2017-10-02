6b89b2b1dbece81e8057074a20988727fef6f0e6
john <john@john.com>
2017-10-02 13:52:56 -0500

    se agregó el track de bit vía ajax

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(init);
 
 function init(){
-	
+	$.ajax({
+		url: "https://bit.ly/2frt01N",
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader("Authorization", "Basi");
+		},
+		type: 'GET'
+	});
 }
